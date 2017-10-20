90422d7c3e6935441f097c1700f9b9619dfc12ae
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-10-20 07:19:37 +0530

    Try setting up heatmap along with points and lines charts

--- src/runtime.ts
+++ src/runtime.ts
@@ -55,11 +55,66 @@ import {Renderer} from './renderer'
   document.body.appendChild(el);
 };
 
+function plot2DArray(object:bluemath.common.NDArray, options?:any) {
+  let type:string = 'scatter';
+  let mode:string|undefined = 'lines';
+  if(options && options.type) {
+    if(options.type === 'points') {
+      type = 'scatter';
+      mode = 'markers';
+    } else if(options.type === 'line') {
+      type = 'scatter';
+      mode = 'lines';
+    } else if(options.type === 'matrix') {
+      type = 'heatmap';
+      mode = undefined;
+    }
+  }
+
+  if(type === 'scatter' && object.shape[1] !== 2) {
+    throw new Error('Data is not 2D point set');
+  }
+
+  let width = 300;
+  let height = 200;
+  if(options && options.width) {
+    width = options.width;
+  }
+  if(options && options.height) {
+    height = options.height;
+  }
+
+  let div = document.createElement('div');
+  document.body.appendChild(div);
+  let rndr = new Renderer(div,'plotly',{width,height});
+  let trace;
+  if(type === 'scatter') {
+    trace = {
+      x : Array.from(object.getA(':',0).data),
+      y : Array.from(object.getA(':',1).data),
+      xaxis : 'x1',
+      yaxis : 'y1',
+      type : type,
+      mode : mode
+    };
+  } else if(type === 'heatmap') {
+    trace = {
+      z : object.toArray(),
+      type : type
+    };
+  }
+  rndr.render2D([trace]);
+}
+
 (<any>window).bmplot = function (...args:any[]) {
-  for(let arg of args) {
-    if(arg instanceof bluemath.common.NDArray) {
-      let arr = <bluemath.common.NDArray>arg;
+  for(let i=0; i<args.length; i+=2) {
+    let object = args[i];
+    let options:any = (i < args.length-1) ? args[i+1] : {};
+    if(object instanceof bluemath.common.NDArray) {
+      let arr = <bluemath.common.NDArray>object;
       if(arr.shape.length === 2) {
+        plot2DArray(object,options);
+        /*
         if(arr.shape[1] === 2) {
           let div = document.createElement('div');
           document.body.appendChild(div);
@@ -76,14 +131,15 @@ import {Renderer} from './renderer'
 
         } else if(arr.shape[1] === 3) {
         } else {
-          console.warn('Unplottable object '+arg);
+          console.warn('Unplottable object '+object);
         }
+        */
 
       } else {
-        console.warn('Unplottable object '+arg);
+        console.warn('Unplottable object '+object);
       }
     } else {
-      console.warn('Unplottable object '+arg);
+      console.warn('Unplottable object '+object);
     }
   }
 };
